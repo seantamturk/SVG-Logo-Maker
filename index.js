49fd8d97329e336ee109b254c1b88ace94d471ea
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-04-24 19:49:33 -0700

    mostly finished shapes.js, createsvg.js and progress on index.js

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
-const { Circle, Square, Triangle} =  require('./lib/shapes')
-
+const { Circle, Square, Triangle } = require('./lib/shapes')
 const SVG = require('./lib/createsvg')
+
+const colorChoices = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
 
 inquirer
     .createPromptModule([
         {
             type: 'list',
-            message: 'Shape type',
+            message: 'Choose a shape type:',
             name: 'shape',
             choices: ['circle', 'square', 'triangle']
         }
+
+        {
+            type: 'input'
+            message: 'Enter text for the logo (up to 3 characters):',
+            name: 'text'
+            validate: (input) => {
+                if (input.length > 3) {
+                    return 'Text must be 3 characters or fewer.';
+                }
+                return true;
+            },
+        },
+        {
+            type: 'list',
+            message: 'Choose a color for the text:',
+            name: 'textColor',
+            choices: colorChoices,
+        },
     ])
+    
     .then((response) => {
         console.log(response.shape)
         let shape;
@@ -21,5 +41,5 @@ inquirer
         const svg = new SVG()
 
         svg.chosenShape(shape)
-        
+
     })
